245cc9394dfab19a379f4053c7993add202aa689
randymarsh77 <mattpsbs@gmail.com>
2024-12-14 11:25:03 -0800

    Improve test coverage.

--- Tests/CancellationTests/CancellationTests.swift
+++ Tests/CancellationTests/CancellationTests.swift
@@ -11,9 +11,29 @@ class CancellationTests: XCTestCase {
 		XCTAssertFalse(token.isCancellationRequested)
 		XCTAssertTrue(token.canBeCanceled)
 
+		var callbackHappenedCount = 0
+		let registration = try? source.register {
+			callbackHappenedCount += 1
+		}
+		XCTAssertNotNil(registration)
+
 		source.cancel()
 
 		XCTAssertTrue(source.isCancellationRequested)
 		XCTAssertTrue(token.isCancellationRequested)
+		XCTAssertEqual(callbackHappenedCount, 1)
+
+		var threwException = false
+		do {
+			try token.throwIfCancellationIsRequested()
+		} catch OperationCanceledException.operationCanceled {
+			threwException = true
+		} catch {
+			XCTFail("Unexpected error: \(error)")
+		}
+
+		XCTAssertTrue(threwException)
+
+		source.dispose()
 	}
 }
